cb0436cf00e97fabb6af6a1885d7dc1603367e3a
Bartosz Wójtowicz <bartosz.wojtowicz@netguru.com>
2022-04-19 07:12:21 +0200

    Do some progress on dashboard and readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# abusive-clauses-dashboard
+PAC - Polish Abusive Clauses Dataset
+============
+''I have read and agree to the terms and conditions'' is one of the biggest lies on the Internet. Consumers rarely read the contracts they are required to accept. We conclude agreements over the Internet daily. But do we know the content of these agreements? Do we check potential unfair statements? On the Internet, we probably skip most of the Terms and Conditions. However, it is essential to remember that we conclude many more contracts. Imagine that we want to buy a house, a car, send our kids to the nursery, open a bank account, or many more. In all these situations, you will need to conclude the contract, but there is a high probability that you will not read the entire agreement with proper understanding. European consumer law aims to prevent businesses from using so-called ''unfair contractual terms'' in their unilaterally drafted contracts and require consumers to accept. In this paper the ''unfair contractual term'' is the equivalent of abusive clause, and could be defined as a clause that is unilaterally imposed by one of the contract's parties, unequally affecting the other or creating a situation of imbalance between the duties and rights of the parties.
+
+On the EU and at the national such as the Polish levels, agencies cannot check possible agreements by hand. Hence, we took the first step to evaluate the possibility of accelerating this process. We created a dataset and machine learning models for partially automating the detection of potentially abusive clauses. Consumer protection organizations and agencies can use these resources to make their work more eﬀective and eﬃcient. Moreover, consumers can automatically analyze contracts and understand what they agree upon.
+
+How to run
+------------
+Using Docker:
+```bash
+docker build . -t pac-dashboard
+docker run -p 8501:8501 pac-dashboard
+```
+Using Python:
+```bash
+pip install -r requirements.txt
+streamlit run views/dataset_overview_view.py
+```
+
+References
+------------
+
+License
+------------
--- views/dataset_overview_view.py
+++ views/dataset_overview_view.py
@@ -1,8 +1,13 @@
+import re
 from pathlib import Path
 from typing import Dict # using python 3.8.5
 
 import pandas as pd
+import plotly.figure_factory as ff
+import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
+from unidecode import unidecode
 
 
 SPLIT_NAMES = ['train', 'dev', 'test']
@@ -11,6 +16,9 @@ SPLIT_NAMES = ['train', 'dev', 'test']
 
 flatten_list = lambda main_list: [item for sublist in main_list for item in sublist]
 
+count_num_of_characters = lambda text: len(re.sub(r'[^a-zA-Z]', '', unidecode(text)))
+
+count_num_of_words = lambda text: len(re.sub(r'[^a-zA-Z ]', '', unidecode(text)).split(' '))
 
 @st.cache
 def load_data() -> Dict[str, pd.DataFrame]:
@@ -24,25 +32,54 @@ DATA_DICT = load_data()
 
 st.title("PAC - Polish Abusive Clauses Dataset")
 
+st.header("Dataset Description")
+st.write(
+"""
+On the EU and at the national such as the Polish levels, agencies cannot check possible agreements by hand. 
+Hence, we took the first step to evaluate the possibility of accelerating this process. 
+We created a dataset and machine learning models for partially automating the detection of potentially abusive clauses. 
+Consumer protection organizations and agencies can use these resources to make their work more eﬀective and eﬃcient. 
+Moreover, consumers can automatically analyze contracts and understand what they agree upon.
+"""
+)
+st.write("Paper: [arxiv](https://arxiv.org/)")
+st.write("Github: [github](https://github.com/CLARIN-PL/abusive-clauses-dashboard)")
+
 st.header("Dataset Statistics")
 
-st.write(f"Total Samples: {sum([len(df) for _, df in DATA_DICT.items()])}")
+st.write(f'Train Samples: {len(DATA_DICT["train"])}')
+st.write(f'Val Samples: {len(DATA_DICT["dev"])}')
+st.write(f'Test Samples: {len(DATA_DICT["test"])}') 
+st.write(f"Total: {sum([len(df) for _, df in DATA_DICT.items()])}")
 
 st.subheader("Class distribution per data split")
-st.bar_chart(pd.DataFrame([df.label.value_counts().rename(k) for k, df in DATA_DICT.items()]))
-
-st.subheader("Mean number of words per observation")
-st.area_chart(
-    pd.DataFrame([(
-                    df.text.apply(lambda x: len(x.split(' ')))
-                    .value_counts()
-                    .rename(k)
-                    .sort_index()
-                    .reindex(list(range(150)))
-                    .fillna(0)
-                    ) for k, df in DATA_DICT.items()]
-                ).T
+df_class_dist = pd.DataFrame([df['class'].value_counts().rename(k) for k, df in DATA_DICT.items()]).reset_index().rename({'index': 'split_name'}, axis=1)
+barchart_class_dist = go.Figure(data=[
+    go.Bar(name='BEZPIECZNE_POSTANOWIENIE_UMOWNE', x=SPLIT_NAMES, y=df_class_dist['BEZPIECZNE_POSTANOWIENIE_UMOWNE'].values),
+    go.Bar(name='KLAUZULA_ABUZYWNA', x=SPLIT_NAMES, y=df_class_dist['KLAUZULA_ABUZYWNA'].values),
+])
+barchart_class_dist.update_layout(
+    barmode='group', 
+    title_text='Barchart - class distribution', 
+    xaxis_title='Split name',
+    yaxis_title='Number of data points'
 )
+st.plotly_chart(barchart_class_dist, use_container_width=True)
+
+st.subheader("Number of words per observation")
+hist_data_num_words = [df['text'].apply(count_num_of_words).values for df in DATA_DICT.values()]
+fig_num_words = ff.create_distplot(hist_data_num_words, SPLIT_NAMES, show_rug=False, bin_size=1)
+fig_num_words.update_traces(nbinsx=100, autobinx=True, selector={'type':'histogram'}) 
+fig_num_words.update_layout(title_text='Histogram - number of words per observation', xaxis_title='Number of words')
+st.plotly_chart(fig_num_words, use_container_width=True)
+
+st.subheader("Character count per observation")
+hist_data_num_chars = [df['text'].apply(count_num_of_characters).values for df in DATA_DICT.values()]
+fig_num_chars = ff.create_distplot(hist_data_num_chars, SPLIT_NAMES, show_rug=False, bin_size=1)
+fig_num_chars.update_traces(nbinsx=100, autobinx=True, selector={'type':'histogram'}) 
+fig_num_chars.update_layout(title_text='Histogram - number of characters per observation', xaxis_title='Number of characters')
+st.plotly_chart(fig_num_chars, use_container_width=True)
+
 
 st.subheader("Top 20 common words per data split")
 for i, col in enumerate(st.columns(3)):
